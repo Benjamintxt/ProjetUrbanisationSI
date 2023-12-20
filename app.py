cc50d29c45d2f33c7d7be76216a0bac8b7f99995
Benjamintxt <benjamincaseych@protonmail.com>
2023-12-20 15:36:43 +0100

    Json files stored via kafka

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 import json
 from flask import Flask, request, jsonify
+from confluent_kafka import Producer
 
 app = Flask(__name__)
 
-@app.route('/webhook/', methods=['POST'])
+# Kafka configuration
+kafka_config = {
+    'bootstrap.servers': 'localhost:9092',  # Replace with your Kafka broker address
+    'client.id': 'flask-kafka-producer'
+}
+
+# Create Kafka producer
+producer = Producer(kafka_config)
+
+@app.route('/webhook', methods=['POST'])
 def webhook_handler():
     try:
         # Get the JSON data from the request
         json_data = request.get_json()
 
-        # Save the JSON data to a file (you can customize the filename and path)
-        with open('webhook_data.json', 'w') as file:
-            file.write(json.dumps(json_data, indent=2))  # Convert to JSON string with indentation
+        # Send the JSON data to Kafka topic
+        producer.produce('webhook_events', json.dumps(json_data))
 
-        return jsonify({'message': 'Webhook data saved successfully'}), 200
+        """ Save the JSON data to a file (you can customize the filename and path)
+        with open('webhook_data.json', 'w') as file:
+            file.write(json.dumps(json_data, indent=2))  # Convert to JSON string with indentation"""
+
+        return jsonify({'message': 'Webhook data saved and sent to Kafka successfully'}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500

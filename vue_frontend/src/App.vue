7cab9b8cf8475f150c2fe518be063a4942d60144
Omar <omar.griggio@he-arc.ch>
2023-12-20 16:19:10 +0100

    Add vue js front end folder

---
+++ vue_frontend/src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March'],
+        datasets: [
+          {
+            label: 'Dashboard',
+            backgroundColor: '#f87979',
+            data: [40, 20, 12]
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div class="chart"><Bar :data="chartData" /></div>
+  </header>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900');
+.chart {
+  width: 100%;
+  height: 100%;
+  max-width: 800px;
+  max-height: 600px;
+  margin: 0 auto;
+}
+</style>
